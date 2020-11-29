5b60bff1c052ba63b08af4d75095f976dd0d34cd
Viet Hua <huaviet999@gmail.com>
2020-11-29 15:00:03 +0700

    init commit

---
+++ data-structure-lab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  data-structure-lab
+//
+//  Created by Viet on 11/29/20.
+//  Copyright © 2020 Viet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
